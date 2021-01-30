26ce9be606d6dd5a2771379a56a24afd057c2e97
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-01-30 18:03:29 +0100

    project is already in remote repo, committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,12 @@
 public class Hello {
     //more stuff
     //mac users and windows users are happy
+
+    /*
+    I have already published on Github.com
+    now I am ready to make more commits
+    but do I need to share again???
+    ABSOLUTELY NO
+    It is time commit and just push!!!!!!
+     */
 }
