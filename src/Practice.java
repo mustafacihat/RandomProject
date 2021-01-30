d0608a6bfa521ae0e6a837e6151da272b276bb26
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-01-30 18:29:03 +0100

    updated Practice.java in local repository

--- src/Practice.java
+++ src/Practice.java
@@ -1,8 +1,8 @@
-public class Practice{
+public class Practice {
 
-public static void main(String [] args){
+    public static void main(String[] args) {
 
-System.out.println("just for practice");
-}
+        System.out.println("just for practice");
+    }
 
 }
